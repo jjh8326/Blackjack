749f6b07a7ec983f2db68e668d1333e69745b08f
Joe Harasz <j30h30@github.com>
2024-09-08 15:23:21 -0400

    Initial Commit

---
+++ JJH-Blackjack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JJH-Blackjack
+//
+//  Created by Joe H on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JJH-Blackjack/JJH_BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJH_BlackjackApp.swift
+//  JJH-Blackjack
+//
+//  Created by Joe H on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct JJH_BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
